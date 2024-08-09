afd6b03f749094b3ff808d3cfd2091bd7adf73a4
Abimanianandan <abikbm06@gmail.com>
2024-08-09 12:32:33 +0530

    add project list

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,62 +1,63 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Project = () => {
+  const list = [
+    {
+      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTytkZ0onGN7I3kFnAnVHfUBo05aRnaPztD6g&s",
+      title:"HR-Management",
+      desc:`Human Resource Management (HRM) focuses on recruiting,
+      developing, and retaining employees to enhance organizational
+      performance. It involves managing employee relations, benefits,
+      and compliance with labor laws. HRM aims to create a motivated
+      workforce aligned with the company's strategic goals.`,
+      link:"https://hr-management-fe.vercel.app/"
+    },
+    {
+      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtVe4gMCR1w8HvOLU2ibivRuUnJ_qtQCem3g&s",
+      title:"Todo-List",
+      desc:`  A lodo list is a simple, often informal list that outlines tasks
+      or items to be accomplished or addressed. It serves as a helpful
+      tool for organizing and prioritizing activities, making it
+      easier to manage daily responsibilities. a lodo list can enhance
+      productivity and ensure nothing important is overlooked.`,
+      link:"https://todo-fe-zeta.vercel.app/"
+    }
+    
+  ]
   return (
     <div className="container mt-5">
       <h1 className="text-dark" style={{ textShadow: "2px 2px purple" }}>
         Project
       </h1>
-      <div class="row mt-5">
-        <div class="col-sm-6 mb-3 mb-sm-0">
-          <div class="card">
+      <div class="row  row-cols-1 row-cols-md-2 g-4 mt-5">
+        { 
+         list.map((item,index)=>{
+          return(
+            <div key={index}>
+                 <div class="col">
+          <div class="card" >
             <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTytkZ0onGN7I3kFnAnVHfUBo05aRnaPztD6g&s"
+              src={item.img}
               alt="HR-img"
+              style={{height:"220px"}}
             />
             <div class="card-body">
-              <h5 class="card-title text-info">HR-Management</h5>
-              <p class="card-text">
-                Human Resource Management (HRM) focuses on recruiting,
-                developing, and retaining employees to enhance organizational
-                performance. It involves managing employee relations, benefits,
-                and compliance with labor laws. HRM aims to create a motivated
-                workforce aligned with the company's strategic goals.
-              </p>
-              <Link
-                to={"https://hr-management-fe.vercel.app/"}
+              <h5 class="card-title text-info">{item.title}</h5>
+              <p class="card-text">{item.desc}</p>
+              <a
+                href={`${item.link}`}
                 class="btn btn-primary"
+                target="blank"
               >
                 Go to Visit
-              </Link>
+              </a>
             </div>
           </div>
         </div>
-        <div class="col-sm-6 mb-3">
-          <div class="card">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtVe4gMCR1w8HvOLU2ibivRuUnJ_qtQCem3g&s"
-              alt="todo-img"
-              style={{ height: "260px" }}
-            />
-            <div class="card-body">
-              <h5 class="card-title text-info">Todo-List</h5>
-              <p class="card-text">
-                A lodo list is a simple, often informal list that outlines tasks
-                or items to be accomplished or addressed. It serves as a helpful
-                tool for organizing and prioritizing activities, making it
-                easier to manage daily responsibilities. a lodo list can enhance
-                productivity and ensure nothing important is overlooked.
-              </p>
-              <Link
-                to={"https://todo-fe-one.vercel.app/"}
-                class="btn btn-primary "
-              >
-                Go to Visit
-              </Link>
             </div>
-          </div>
-        </div>
+          )
+         })      
+        }
       </div>
     </div>
   );
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ResumeImage from "../assets/My image/resume.png";
+import ResumeImage from "../assets/My image/resume.jpeg";
 
 const Resume = () => {
   return (
@@ -7,7 +7,7 @@ const Resume = () => {
       <h1 className="text-warning" style={{ textShadow: "2px 2px purple" }}>
         Resume
       </h1>
-      <img src={ResumeImage} alt="resume img" />
+      <img src={ResumeImage} alt="resume img" style={{width:"900px"}}/>
     </div>
   );
 };
